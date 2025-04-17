b9982b64f3db4d6a91d1adc9fed42dbe6dbd345d
PanicAthe <ekdekdgkrp20@gmail.com>
2025-04-17 09:35:54 +0900

    [level 2] Title: 점 찍기, Time: 17.01 ms, Memory: 92.7 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.java
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -1,9 +1,8 @@
 class Solution {
     public long solution(int k, int d) {
         long answer = 0;
-        long l = d;
-        for(long i=0; i<=l;i+=k){
-            answer += (long)Math.sqrt((long)l*l-(long)i*i)/k + 1;
+        for(long i=0; i<=d;i+=k){
+            answer += (long)Math.sqrt((long)d*d-(long)i*i)/k + 1;
         }
         return answer;
     }
