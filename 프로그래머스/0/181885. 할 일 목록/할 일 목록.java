eab11358bb92d44dc8c4a05de11f4cfe0052e3ba
PanicAthe <ekdekdgkrp20@gmail.com>
2025-04-21 17:41:42 +0900

    [level 0] Title: 할 일 목록, Time: 23.51 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -1,18 +1,11 @@
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.Arrays;
 
 class Solution {
     public String[] solution(String[] todo_list, boolean[] finished) {
-        List<String> list = new ArrayList<String>();
-        for(int i=0; i<todo_list.length; i++) {
-            if(!finished[i]) {
-                list.add(todo_list[i]);
-            }
-        }
-        String[] result = new String[list.size()];
-        for(int i=0; i<list.size(); i++) {
-            result[i] = list.get(i);
-        }
-        return result;
+        return Arrays.stream(todo_list)   // todo_list를 스트림으로 변환
+                     .filter(i -> !finished[Arrays.asList(todo_list).indexOf(i)])  // finished[i]가 false인 것만 필터링
+                     .toArray(String[]::new);   // 필터링된 결과를 String 배열로 변환
     }
 }
