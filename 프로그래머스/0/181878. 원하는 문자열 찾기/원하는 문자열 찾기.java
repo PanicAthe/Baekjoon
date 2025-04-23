f559014356f696eeb06e885651bab0145d1ca9df
PanicAthe <ekdekdgkrp20@gmail.com>
2025-04-23 14:06:36 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 26.02 ms, Memory: 111 MB -BaekjoonHub

---
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        myString = myString.toLowerCase();
+        pat = pat.toLowerCase();
+        for(int i = 0; i < myString.length()-pat.length()+1; i++) {
+            if(myString.substring(i, i+pat.length()).equals(pat)) {
+                return 1;
+            }
+        }
+        return 0;
+    }
+}
