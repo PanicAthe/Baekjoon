9e0e6200028e2dfafedbf65d77c3d3802a10d2b8
PanicAthe <ekdekdgkrp20@gmail.com>
2025-05-09 11:35:23 +0900

    [level 0] Title: OX퀴즈, Time: 0.20 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String[] solution(String[] quiz) {
+        String[] answer = new String[quiz.length];
+        for (int i = 0; i < quiz.length; i++) {
+            String[] words = quiz[i].split(" ");
+            int x = Integer.parseInt(words[0]);
+            String op = words[1];
+            int y = Integer.parseInt(words[2]);
+            int z = Integer.parseInt(words[4]);
+            if(op.equals("-")) {
+                if(x-y==z){
+                    answer[i] = "O";
+                }else{
+                    answer[i] = "X";
+                }
+            }else if(op.equals("+")) {
+                if(x+y==z){
+                    answer[i] = "O";
+                }else{
+                    answer[i] = "X";
+                }
+            }
+        }
+        return answer;
+    }
+}
