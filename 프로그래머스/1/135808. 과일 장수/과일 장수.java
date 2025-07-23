5c760cb1c64e4aabffe5d7f110d91d5dc778412d
PanicAthe <ekdekdgkrp20@gmail.com>
2025-07-23 11:07:57 +0900

    [level 1] Title: 과일 장수, Time: 55.99 ms, Memory: 135 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -2,15 +2,15 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int k, int m, int[] score) {
-        // 점수 내림차순 정렬
+        // 점수 내림차순 
         Arrays.sort(score);
         
         int sum = 0;
         int n = score.length;
         
-        // 큰 점수부터 m개씩 묶어서 최소값 * m
+        // 큰 점수부터 m개씩 
         for (int i = n - m; i >= 0; i -= m) {
-            sum += score[i] * m; // score[i]가 해당 묶음 중 최소값
+            sum += score[i] * m; 
         }
         
         return sum;
