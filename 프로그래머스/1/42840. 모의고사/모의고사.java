3239ca20defe4d6a91d373c7e99fa565250ae3ad
PanicAthe <panicathe@naver.com>
2025-11-08 22:50:11 +0900

    [level 1] Title: 모의고사, Time: 6.08 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,39 +1,29 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(int[] answers) {
-        int[] answer = new int[3];
-
-        int[][] students = {
-            {1, 2, 3, 4, 5}, // Student 1
-            {2, 1, 2, 3, 2, 4, 2, 5}, // Student 2
-            {3, 3, 1, 1, 2, 2, 4, 4, 5, 5} // Student 3
+        int[][] pattern = {
+            {1,2,3,4,5},
+            {2,1,2,3,2,4,2,5},
+            {3,3,1,1,2,2,4,4,5,5}
         };
-
-        for(int i=0; i<answers.length; i++) {
-            for(int j=0; j<students.length; j++) {
-                if(answers[i] == students[j][i % students[j].length]) {
-                    answer[j]++;
+        
+        int[] scores = new int[3];
+        
+        for(int i=0; i<answers.length; i++){
+            for(int j=0; j<pattern.length; j++){
+                if(answers[i]==pattern[j][i%pattern[j].length]){
+                    scores[j]++;
                 }
             }
         }
-
-        if(answer[0]>answer[1] && answer[0]>answer[2]) {
-            return new int[]{1};
-        } else if(answer[1]>answer[0] && answer[1]>answer[2]) {
-            return new int[]{2};
-        } else if(answer[2]>answer[0] && answer[2]>answer[1]) {
-            return new int[]{3};
-        } else {
-            int maxScore = Math.max(Math.max(answer[0], answer[1]), answer[2]);
-            List<Integer> winners = new ArrayList<>();
-            for (int i = 0; i < answer.length; i++) {
-                if (answer[i] == maxScore) {
-                    winners.add(i + 1);
-                }
+        
+        int maxScore = Arrays.stream(scores).max().getAsInt();
+        ArrayList<Integer> answer = new ArrayList<>();
+        for(int i=0;i<scores.length;i++){
+            if(scores[i]==maxScore){
+                answer.add(i+1);
             }
-            return winners.stream().mapToInt(i -> i).toArray();
         }
-
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
