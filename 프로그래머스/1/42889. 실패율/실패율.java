2a2cc4d355f2c56e7279c40ac452587fc76386b8
PanicAthe <panicathe@naver.com>
2025-11-11 21:06:28 +0900

    [level 1] Title: 실패율, Time: 13.83 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -2,54 +2,27 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int N, int[] stages) {
-        Stage[] stageArray = new Stage[N];
-        int userCount = stages.length;
-
-        for (int i = 0; i < N; i++) {
-            stageArray[i] = new Stage(i + 1); 
+        int[] counts = new int[N+1]; // 스테이지들의 현재 사용자 count
+        
+        for(int i=0; i<stages.length; i++){
+            counts[stages[i]-1]++;
         }
-
-        // 각 스테이지 사람 count
-        for (int stage : stages) {
-            if (stage <= N) {
-                stageArray[stage - 1].stay++;
+        
+        HashMap<Integer, Double> map = new HashMap<>();
+        int count = stages.length;  // 앞 스테이지에 있는 사용자 count
+        for(int i=0; i<counts.length-1;i++){
+            if(counts[i]==0){
+                map.put(i+1, 0.0);
             }
-        }
-
-        // 실패율 
-        for (int i = 0; i < N; i++) {
-            if (userCount == 0) {
-                stageArray[i].failRate = 0;
-            } else {
-                stageArray[i].failRate = (double) stageArray[i].stay / userCount;
+            else{
+                map.put(i+1, (double)counts[i]/count);
             }
-            userCount -= stageArray[i].stay;
+            count -= counts[i];
         }
-
-        Arrays.sort(stageArray, (a, b) -> {
-            if (Double.compare(b.failRate, a.failRate) != 0) {
-                return Double.compare(b.failRate, a.failRate); // 실패율 우선
-            } else {
-                return Integer.compare(a.number, b.number); // 스테이지 번호 오름차순
-            }
-        });
-
-        int[] result = new int[N];
-        for (int i = 0; i < N; i++) {
-            result[i] = stageArray[i].number;
-        }
-
-        return result;
-    }
-
-    // 스테이지 클래스
-    class Stage {
-        int number;      
-        int stay = 0;    
-        double failRate; 
-
-        Stage(int number) {
-            this.number = number;
-        }
+        
+        return map.entrySet().stream().sorted((o1,o2) -> 
+                o1.getValue().equals(o2.getValue()) ? Integer.compare(o1.getKey(), o2.getKey()) :
+                                             Double.compare(o2.getValue(), o1.getValue())).mapToInt(Map.Entry::getKey).toArray();
+        
     }
 }
