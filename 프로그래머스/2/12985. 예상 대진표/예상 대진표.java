c1d4da897160ed6437a7c626b0d98b176fd87d1a
PanicAthe <panicathe@naver.com>
2025-12-18 16:17:04 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 88.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,21 @@
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int answer = 0;
+ 
+        while(a!=b){
+            answer++;
+            if(a%2==1){
+                a+=1;
+            }
+            if(b%2==1){
+                b+=1;
+            }
+            a/=2;
+            b/=2;
+        }
+
+        return answer;
+    }
+}
