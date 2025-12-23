f4264ac2ac0e00af0b9b545e3f61cfc64695f480
PanicAthe <panicathe@naver.com>
2025-12-23 11:58:40 +0900

    [level 1] Title: 폰켓몬, Time: 6.91 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,17 +1,11 @@
 import java.util.*;
+import java.util.stream.Collectors;
+
 
 class Solution {
     public int solution(int[] nums) {
-        Set<Integer> set = new HashSet<>();
         
-        for(int n : nums){
-            set.add(n);
-        }
-        
-        if(set.size()>=nums.length/2){
-            return nums.length/2;
-        }
-        
-        return set.size();
+        Set<Integer> set = Arrays.stream(nums).boxed().collect(Collectors.toCollection(HashSet::new));       
+        return Math.min(nums.length/2, set.size());
     }
 }
