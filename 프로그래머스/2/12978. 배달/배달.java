f67c0e6debbeaacb892d719fa368b548c2ee127d
PanicAthe <panicathe@naver.com>
2025-12-30 17:34:25 +0900

    [level 2] Title: 배달, Time: 3.34 ms, Memory: 89.2 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -36,8 +36,8 @@ class Solution {
             graph.get(b).add(new Node(a, c));
         }
 
-        // 4. 최단 거리(시간)를 저장할 배열 초기화
-        // dist[i]는 1번 마을에서 i번 마을까지 가는 '최소 시간'
+        // 4. 최단 거리를 저장할 배열 초기화
+        // dist[i]는 1번 마을에서 i번 마을까지 가는 '최소 거리'
         int[] dist = new int[N + 1];
         Arrays.fill(dist, Integer.MAX_VALUE); // 처음엔 모두 무한대로 초기화
         dist[1] = 0; // 시작점(1번)의 시간은 0
@@ -48,20 +48,20 @@ class Solution {
 
         while (!pq.isEmpty()) {
             Node current = pq.poll(); // 가장 비용이 적은 노드 꺼내기 
-
-            // 현재 꺼낸 경로의 비용이 이미 기록된 최소 비용보다 크다면 계산할 필요 없음
-            // (이미 더 빠른 길을 찾았다는 뜻)
-            if (current.cost > dist[current.idx]) continue;
-
-            // 현재 위치와 연결된 인접 마을들 확인
-            for (Node next : graph.get(current.idx)) {
-                // '현재까지 온 비용 + 다음 마을로 가는 비용'이 '기존에 알고 있던 다음 마을 도착 비용'보다 싸다면 갱신
-                if (dist[next.idx] > current.cost + next.cost) {
-                    dist[next.idx] = current.cost + next.cost;
-                    // 갱신된 정보를 큐에 넣어줌 (새로운 최단 경로 후보)
+            
+            // 1번 마을에서 현재 마을까지의 거리가 이미 등록된 최단 거리보다 크다면 패스
+            if(current.cost>dist[current.idx]) continue;
+            
+            // 현재 마을에서 갈 수 있는 모든 경로 탐색
+            for(Node next : graph.get(current.idx)){
+                // 1번 마을에서 현재 마을 + 다음 마을 거리가 1번마을에서 다음 마을 거리보다 작다면
+                if(dist[next.idx] > next.cost + current.cost){
+                    dist[next.idx] = next.cost + current.cost;
+                    // 업데이트된 최단 거리 다시 큐에 넣기
                     pq.offer(new Node(next.idx, dist[next.idx]));
                 }
-            }
+            }            
+            
         }
 
         // 6. K시간 이하로 배달 가능한 마을 개수 세기
