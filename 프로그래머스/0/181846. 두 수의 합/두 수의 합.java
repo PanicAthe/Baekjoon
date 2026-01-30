1181c02cbca447cba056829d4ca52e427d857492
PanicAthe <panicathe@naver.com>
2026-01-30 13:51:45 +0900

    [level 0] Title: 두 수의 합, Time: 18.30 ms, Memory: 95.3 MB -BaekjoonHub

--- 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
@@ -1,48 +1,36 @@
+import java.util.*;
+
 class Solution {
     public String solution(String a, String b) {
+        //뒷자리부터 올리면서 더하기 
+        
         int a_index = a.length() - 1;
         int b_index = b.length() - 1;
-        String result = "";
-        int cast = 0;
-        while (a_index > -1 && b_index > -1) {
-            int temp = a.charAt(a_index--) - '0';
-            temp += b.charAt(b_index--) - '0';
-            if (temp + cast < 10) {
-                result += (temp + cast) + "";
-            } else {
-                result += (temp + cast) % 10 + "";
+        
+        int carry = 0;
+        int digit = 0;
+        
+        StringBuilder sb = new StringBuilder("");
+        
+        while(a_index >= 0 || b_index >= 0){
+            
+            if(a_index >= 0){
+                carry += a.charAt(a_index--) - '0';
             }
-            cast = (temp + cast) / 10;
+            
+            if(b_index >= 0){
+                carry += b.charAt(b_index--) - '0';
+            }
+            
+            digit = carry % 10;
+            carry = carry / 10;
+            sb.append(digit);
+        }
+        if (carry > 0) {
+            sb.append(carry);
         }
 
-        if (a_index > -1) {
-            while (a_index > -1) {
-                int temp = a.charAt(a_index--) - '0';
-                if (temp + cast < 10) {
-                    result += (temp + cast) + "";
-                } else {
-                    result += (temp + cast) % 10 + "";
-                }
-                cast = (temp + cast) / 10;
-            }
-        } else if (b_index > -1) {
-            while (b_index > -1) {
-                int temp = b.charAt(b_index--) - '0';
-                if (temp + cast < 10) {
-                    result += (temp + cast) + "";
-                } else {
-                    result += (temp + cast) % 10 + "";
-                }
-                cast = (temp + cast) / 10;
-            }
-        }
-        if(cast!=0){
-            result+=cast+"";
-        }
-        String s = "";
-        for (int i = result.length() - 1; i > -1; i--) {
-            s += result.charAt(i);
-        }
-        return s;
+        sb.reverse();
+        return sb.toString();
     }
 }
