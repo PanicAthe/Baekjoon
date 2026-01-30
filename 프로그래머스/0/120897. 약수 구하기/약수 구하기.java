3a42826f2ba28a596542e9aa3084c64a56ac2d0f
PanicAthe <panicathe@naver.com>
2026-01-30 14:22:01 +0900

    [level 0] Title: 약수 구하기, Time: 10.48 ms, Memory: 90.4 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -1,16 +1,18 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 class Solution {
     public int[] solution(int n) {
         List<Integer> list = new ArrayList<>();
-        for (int i = 1; i <= n/2; i++) {
+        for (int i = 1; i <= Math.sqrt(n); i++) {
             if(n%i==0){
                 list.add(i);
+                if(n/i!=i){
+                    list.add(n/i);
+                }
             }
         }
-        list.add(n);
-        return list.stream().mapToInt(i->i).toArray();
+        list.sort((a,b) -> a - b);
+        return list.stream().mapToInt(i -> i).toArray();
     }
 }
