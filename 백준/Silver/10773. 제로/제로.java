aefc980e582124963f521eb404e7576616ca4188
PanicAthe <panicathe@naver.com>
2026-02-01 13:28:11 +0900

    [Silver IV] Title: 제로, Time: 216 ms, Memory: 23876 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+        for(int i=0; i<n; i++){
+            int num = Integer.parseInt(br.readLine());
+            if(num==0){
+                stack.pop();
+            }else{
+                stack.push(num);
+            }
+        }
+
+        int sum = 0;
+        while(!stack.isEmpty()){
+            sum += stack.pop();
+        }
+
+        System.out.print(sum);
+        
+    }
+}
