83c5eac0d92f292fa42530dc5a80fab4eab428bd
PanicAthe <panicathe@naver.com>
2026-02-19 18:59:44 +0900

    [Silver II] Title: 돌 게임 4, Time: 108 ms, Memory: 14320 KB -BaekjoonHub

---
+++ 백준/Silver/9658. 돌 게임 4/돌 게임 4.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        // i 개의 돌이 주어졌을 때, 시작하는 사람이 이길지 체크
+        boolean[] dp = new boolean[N+1];
+        
+        // 내 차례에서 가져갈 돌이 없음 = 승리
+        dp[0] = true;
+
+        for(int i=1; i<=N; i++){
+            boolean check = false;
+            
+            // i-1 이 false라면 돌 1개를 가져가고 내가 이길 수 있음 
+            if(!dp[i-1]){
+                check = true;
+            }
+
+            // i-3 이 false라면 돌 3개를 가져가고 내가 이길 수 있음 
+            if(i>=3 && !dp[i-3]){
+                check = true;
+            }
+
+            // i-4 이 false라면 돌 4개를 가져가고 내가 이길 수 있음
+            if(i>=4 && !dp[i-4]){
+                check = true;
+            }
+
+            dp[i] = check;
+        }
+
+        if(dp[N]){
+            System.out.print("SK");
+        }else{
+            System.out.print("CY");
+        }
+    }
+}
