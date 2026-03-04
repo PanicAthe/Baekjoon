24baf01e05e99a0d5f6ba70da6c72637db553bd4
PanicAthe <panicathe@naver.com>
2026-03-04 17:58:39 +0900

    [level 4] Title: 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -0,0 +1,23 @@
+-- 이블에서 자동차 종류가 '세단' 또는 'SUV' 인 자동차 중 
+-- 2022년 11월 1일부터 2022년 11월 30일까지 대여 가능하고 
+-- 30일간의 대여 금액이 50만원 이상 200만원 미만인 자동차에 대해서 
+-- 자동차 ID, 자동차 종류, 대여 금액(컬럼명: FEE) 리스트를 출력하는 SQL문을 작성해주세요. 
+
+SELECT C.CAR_ID,
+       C.CAR_TYPE,
+       FLOOR(C.DAILY_FEE * 30 * (100 - P.DISCOUNT_RATE) / 100) AS FEE
+FROM CAR_RENTAL_COMPANY_CAR C
+JOIN CAR_RENTAL_COMPANY_DISCOUNT_PLAN P
+  ON C.CAR_TYPE = P.CAR_TYPE
+WHERE C.CAR_TYPE IN ('세단', 'SUV')
+  AND P.DURATION_TYPE = '30일 이상'
+  AND NOT EXISTS (
+        SELECT 1
+        FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY H
+        WHERE H.CAR_ID = C.CAR_ID
+          AND H.START_DATE <= '2022-11-30'
+          AND H.END_DATE >= '2022-11-01'
+      )
+  AND FLOOR(C.DAILY_FEE * 30 * (100 - P.DISCOUNT_RATE) / 100) >= 500000
+  AND FLOOR(C.DAILY_FEE * 30 * (100 - P.DISCOUNT_RATE) / 100) < 2000000
+ORDER BY FEE DESC, C.CAR_TYPE ASC, C.CAR_ID DESC;
