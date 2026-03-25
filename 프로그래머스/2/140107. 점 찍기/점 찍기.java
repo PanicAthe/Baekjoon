752a708150f05f1241642d3ba1a3db0d7428a016
PanicAthe <panicathe@naver.com>
2026-03-25 23:32:17 +0900

    [level 2] Title: 점 찍기, Time: 13.60 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.java
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -4,9 +4,16 @@ class Solution {
     public long solution(int k, int d) {
         long answer = 0;
         
-        for(int i=0; i<=d/k; i++){
+        // 좌표평면 x, y 값은 k의 배수 (0, 1*k, 2*k ...)
+        // 원점(0,0)과 거리가 d를 넘지 않음
+        
+        for(int i=0; i*k<=d; i++){
             long x = i*k;
+            
+            // 현재 x에서 가능한 y의 최대값
             long maxY = (long) Math.sqrt((long) d*d - x*x);
+            
+            // y도 k의 배수
             answer += (int) maxY / k + 1;
         }
         
