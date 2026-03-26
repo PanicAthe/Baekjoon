e131eb0f994a099b13cd51ff61ebf49d89268513
PanicAthe <panicathe@naver.com>
2026-03-26 21:16:24 +0900

    [level 3] Title: N으로 표현, Time: 32.97 ms, Memory: 93.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int N, int number) {
+        
+        // dp[i] : 1번 숫자 N를 포함해서 만들 수 있는 수 집합
+        Set<Integer>[] dp = new HashSet[9];
+        
+        for(int i=1; i<=8; i++){
+            dp[i] = new HashSet<>();
+            
+            // N, NN, NNN
+            int n = 0;
+            for(int j=0; j<i; j++){
+                n = n*10 + N;
+            }
+            dp[i].add(n);
+            
+            // j번 숫자 N를 포함해서 만들 수 있는 수의 집합과
+            // i-j번 숫자 N를 포함해서 만들 수 있는 수 집합의 조합
+            for(int j=1; j<i; j++){
+                for(int a: dp[j]){
+                    for(int b: dp[i-j]){
+                        dp[i].add(a+b);
+                        dp[i].add(a-b);
+                        dp[i].add(a*b);
+                        if(b!=0) dp[i].add(a/b);
+                    }
+                }
+            }
+            
+            if(dp[i].contains(number))
+                return i;
+        }
+        
+        return -1;
+    }
+}
