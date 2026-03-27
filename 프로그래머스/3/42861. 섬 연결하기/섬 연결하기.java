905135e38ce1f51465d598b725393fca54e48543
PanicAthe <panicathe@naver.com>
2026-03-27 20:33:44 +0900

    [level 3] Title: 섬 연결하기, Time: 0.56 ms, Memory: 70.5 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -4,17 +4,15 @@ class Solution {
     
     static int[] parent;
     
-    public static int find(int x){
-        // x가 루트라면 x 반환
-        if(parent[x]==x) return x;
-        // x의 루트를 찾아서 값 업데이트 후 반환
+    // 본인 루트 찾는 메소드
+    static int find(int x){
+        if(parent[x] == x) return x;
+        // 루트 찾는 경로 압축
         return parent[x] = find(parent[x]);
     }
-       
+    
     class Edge implements Comparable<Edge>{
-        int from;
-        int to;
-        int cost;
+        int from, to, cost;
         
         Edge(int from, int to, int cost){
             this.from = from;
@@ -22,7 +20,6 @@ class Solution {
             this.cost = cost;
         }
         
-        // 비용 오름차순
         @Override
         public int compareTo(Edge o){
             return this.cost - o.cost;
@@ -31,31 +28,36 @@ class Solution {
     
     public int solution(int n, int[][] costs) {
         
-        // 루트 초기화
-        parent = new int[n+1];
-        for(int i=1; i<=n; i++){
-            parent[i] = i;
-        }
-
-        ArrayList<Edge> list = new ArrayList<>();
+        // n개의 섬을 잇는 최소 다리 건설 비용 구하기
         
+        // costs[i][0] : from
+        // costs[i][1] : to
+        // costs[i][2] : cost, 다리 건설 비용
+        
+         // 간선들을 건설 비용 오름차순으로 정리
+        ArrayList<Edge> list = new ArrayList<>();
         for(int i=0; i<costs.length; i++){
             list.add(new Edge(costs[i][0], costs[i][1], costs[i][2]));
         }
+        list.sort(null);
         
-        Collections.sort(list);
+        // 부모 초기화
+        parent = new int[n];
+        for(int i=0; i<n; i++){
+            parent[i] = i;
+        }
         
         int answer = 0;
         
-        // 비용이 작은 순서대로 다리 놓기 (사이클은 제외)
         for(Edge edge : list){
             int rootX = find(edge.from);
             int rootY = find(edge.to);
             
-            // 사이클이 생기지 않는 경우
+            // 루트가 달라 사이클이 생기지 않음
             if(rootX != rootY){
-                parent[rootY] = rootX;
                 answer += edge.cost;
+                // 다리 연결
+                parent[rootX] = rootY;
             }
         }
         
