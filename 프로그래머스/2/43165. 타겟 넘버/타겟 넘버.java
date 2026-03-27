876c3bc0621103e7a5a3c132a11ff704d978e9a6
PanicAthe <panicathe@naver.com>
2026-03-27 21:13:02 +0900

    [level 2] Title: 타겟 넘버, Time: 4.72 ms, Memory: 69.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,30 +1,32 @@
-class Solution {
+import java.util.*;
 
-    static int n, t, answer;
-    static int[] nums;
-    
-    static void dfs(int index, int sum){
-        if(index==n){
-            if(sum==t){
-                answer++;
-            }
-            return;
-        }else{
-            dfs(index+1, sum-nums[index]);
-            dfs(index+1, sum+nums[index]);
-        }
-    }
-        
+class Solution {
     public int solution(int[] numbers, int target) {
         
-        // numbers 를 활용하여 +, - 연산을 통해 target 만드는 경우의 수
-        // numbers 의 순서를 바꾸지 않고 모두 사용해야 함
+        // dp: (현재까지 만들 수 있는 합, 그 합을 만드는 경우의 수)
+        Map<Integer, Integer> dp = new HashMap<>();
         
-        answer = 0;
-        n = numbers.length;
-        t = target;
-        nums = numbers;
-        dfs(0, 0);
-        return answer;
+        dp.put(0, 1); // 합 0을 만드는 방법은 1가지 (초기 상태)
+
+        for (int num : numbers) {
+            // 현재 숫자까지 고려했을 때의 결과 저장
+            Map<Integer, Integer> next = new HashMap<>();
+
+            // 이전 단계의 모든 합에 대해
+            for (int sum : dp.keySet()) {
+                
+                // 해당 합을 만들 수 있는 경우의 수
+                int count = dp.get(sum);
+                
+                // 현재 숫자를 더하거나 빼서 새로운 합 생성
+                // 이미 같은 합이 존재하면 경우의 수 누적
+                next.put(sum + num, next.getOrDefault(sum + num, 0) + count);
+                next.put(sum - num, next.getOrDefault(sum - num, 0) + count);
+            }
+
+            dp = next; 
+        }
+
+        return dp.getOrDefault(target, 0);
     }
 }
