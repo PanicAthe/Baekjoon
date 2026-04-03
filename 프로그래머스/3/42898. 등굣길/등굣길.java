0a69b287a912920686cf905f386f099f1d3e81af
PanicAthe <panicathe@naver.com>
2026-04-03 18:09:54 +0900

    [level 3] Title: 등굣길, Time: 0.82 ms, Memory: 53.8 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,30 +1,34 @@
 class Solution {
-    public int solution(int m, int n, int[][] puddles) {  
+    public int solution(int m, int n, int[][] puddles) {
+        int mod = 1_000_000_007;
         
-        // dp[i][j] : (i, j) 좌표까지의 최단 거리 경로 개수
-        int[][] dp = new int[m][n];
+        // 집에서 학교까지 갈 수 있는 최단 경로의 개수 
+        // 오른쪽과 아래쪽으로만 움직임 
+        // -> 되돌아가거나 사이클이 생길 일이 없음. = 모든 경로가 최단 경로
+        
+        // 집: 0행, 0열
+        // 학교: n-1 행, m-1 열
+        
+        int[][] dp = new int[n][m];
+        
+        for(int i=0; i<puddles.length; i++){
+            dp[puddles[i][1]-1][puddles[i][0]-1] = -1;
+        }
         
         dp[0][0] = 1;
         
-        // 물 웅덩이 표시
-        for(int i=0; i<puddles.length; i++){
-            dp[puddles[i][0]-1][puddles[i][1]-1] = -1;
-        }
-        
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(dp[i][j]==-1){
-                    continue;
-                }
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(dp[i][j]==-1) continue;
                 
-                // 위쪽 경로 더하기
-                if(i>0 && dp[i-1][j]!=-1) dp[i][j] = (dp[i][j]+dp[i-1][j])%1_000_000_007;
+                // 위에서 오는 경로 수 합치기
+                if(i>0 && dp[i-1][j] != -1) dp[i][j] = (dp[i][j] + dp[i-1][j]) % mod;
                 
-                // 왼쪽 경로 더하기
-                if(j>0 && dp[i][j-1]!=-1) dp[i][j] = (dp[i][j]+dp[i][j-1])%1_000_000_007;
+                // 왼쪽에서 오는 경로의 수 합치기
+                if(j>0 && dp[i][j-1] != -1) dp[i][j] = (dp[i][j] + dp[i][j-1]) % mod;
             }
         }
         
-        return dp[m-1][n-1];
+        return dp[n-1][m-1];
     }
 }
