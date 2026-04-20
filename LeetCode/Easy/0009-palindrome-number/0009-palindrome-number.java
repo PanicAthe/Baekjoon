cb6d6827b68abe0245526d5d779b08097dd8cf27
PanicAthe <panicathe@naver.com>
2026-04-20 15:54:26 +0900

    Time: 6 ms (23.17%), Space: 45.8 MB (75.53%) - LeetHub

---
+++ LeetCode/Easy/0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public boolean isPalindrome(int x) {
+        String s = String.valueOf(x);
+        for(int i=0; i<s.length()/2; i++){
+            if(s.charAt(i)!=s.charAt(s.length()-i-1)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
