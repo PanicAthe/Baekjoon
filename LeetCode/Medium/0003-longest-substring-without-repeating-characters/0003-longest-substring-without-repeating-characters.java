d509fa5fdbca21fc4bedb463f22e914721b9b0d2
PanicAthe <panicathe@naver.com>
2026-04-22 10:36:58 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ LeetCode/Medium/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,24 +1,24 @@
 import java.util.*;
 
 class Solution {
-
     public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        int left = 0;
         int result = 0;
-        for(int i=0; i<s.length(); i++){
-            HashMap<Character, Integer> set = new HashMap<>();
-            int cur = 0;
-            for(int j=i; j<s.length(); j++){
-                char c = s.charAt(j);
-                if(set.containsKey(c)){
-                    i = set.get(c);
-                    break;
-                }else{
-                    cur++;
-                    set.put(c, j);
-                }
+
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+
+            // 이미 본 문자라면 left를 점프
+            if (map.containsKey(c)) {
+                left = Math.max(left, map.get(c) + 1);
             }
-            result = Math.max(result, cur);
+
+            map.put(c, right);
+
+            result = Math.max(result, right - left + 1);
         }
+
         return result;
     }
 }
